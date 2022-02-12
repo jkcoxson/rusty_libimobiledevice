5690479910566ea3e5826faeeea38925779c06d1
jkcoxson <jkcoxson@gmail.com>
2022-02-12 15:33:22 -0700

    Change wrapper to point to submodules

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 
 extern crate bindgen;
 
-use std::{env, path::PathBuf, fs::canonicalize };
+use std::{env, path::PathBuf, fs::canonicalize, fs::create_dir };
 
 fn main() {
     // Tell cargo to invalidate the built crate whenever the wrapper changes
@@ -25,9 +25,9 @@ fn main() {
         // bindings for.
         .header("wrapper.h")
         // Include in clang build
-        .clang_arg("-I./libimobiledevice/include")
-        .clang_arg("-I./libplist/include")
-        .clang_arg("-I./libimobiledevice")
+        .clang_arg("-I./submodules/libimobiledevice/include")
+        .clang_arg("-I./submodules/libplist/include")
+        .clang_arg("-I./submodules/libimobiledevice")
         .clang_arg(format!("-I{}", gnutls_path))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
@@ -46,45 +46,158 @@ fn main() {
     // Check if folder ./override exists
     let override_path = PathBuf::from("./override");
     if override_path.exists() {
-        println!("cargo:rustc-link-search={}", canonicalize(override_path).unwrap().display());
+        println!("cargo:rustc-link-search={}", canonicalize(&override_path).unwrap().display());
+        // Search in every folder within override
+        for entry in override_path.read_dir().unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_dir() {
+                println!("cargo:rustc-link-search={}", canonicalize(path).unwrap().display());
+            }
+        }
+
     } else {
-        ////////////////////////////
-        // LIBIMOBILEDEVICE BUILD //
-        ////////////////////////////
+        // Get the target triple
+        let target_triple = env::var("TARGET").unwrap();
+        let host_triple = env::var("HOST").unwrap();
 
-        // Change directory to libimobiledevice
-        let _ = env::set_current_dir("./libimobiledevice");
-        // Run ./autogen.sh
-        let _ = std::process::Command::new("sh")
-            .arg("autogen.sh --make-static")
-            .status();
-        // Run make
-        let _ = std::process::Command::new("make").status();
-
-        // Get path to libimobiledevice/src/.libs
-        let libs_path = env::current_dir().unwrap().join("src").join(".libs");
-
-        // Tell cargo to link the library at src/.libs/libimobiledevice.a
-        println!("cargo:rustc-link-search={}", libs_path.display());
+        let ldlibs = "";
 
         ////////////////////////////
         //     LIBPLIST BUILD     //
         ////////////////////////////
 
-        // Change directory to libplist
-        env::set_current_dir("../libplist").expect("Could not change directory to libplist");
+        // Change directory to libimobiledevice
+        let _ = env::set_current_dir("./submodules/libplist");
+        // Create build folder
+        let _ = create_dir("./build");
+        let build_path = PathBuf::from("./build");
         // Run ./autogen.sh
         let _ = std::process::Command::new("sh")
-            .arg("autogen.sh --without-cython --make-static")
+            .arg(format!("./autogen.sh"))
+            .arg("--enable-static")
+            .arg(format!("--prefix={}", canonicalize(build_path.clone()).unwrap().display()))
+            .arg(format!("--build={}", host_triple))
+            .arg(format!("--host={}", target_triple))
             .status();
         // Run make
         let _ = std::process::Command::new("make").status();
+        let _ = std::process::Command::new("make").arg("install").status();
+        // Add the include path to $CLFLAGS
+        add_cflag(&format!("-I{}", canonicalize(&build_path.join("include")).unwrap().display()));
+        // Add lib to ldlibs
+        add_ldlib(&format!("-L{}", canonicalize(&build_path.join("lib")).unwrap().display()));
+        // Change directory back to project root
+        let _ = env::set_current_dir("../../");
 
-        // Get path to libplist/src/.libs
-        let libs_path = env::current_dir().unwrap().join("src").join(".libs");
+        ////////////////////////////
+        //    LIBUSBMUXD BUILD    //
+        ////////////////////////////
 
-        // Tell cargo to link the library at src/.libs/libplist.a
-        println!("cargo:rustc-link-search={}", libs_path.display());
+        // Change directory to libimobiledevice
+        let _ = env::set_current_dir("./submodules/libusbmuxd");
+        // Create build folder
+        let _ = create_dir("./build");
+        let build_path = PathBuf::from("./build");
+        // Run ./autogen.sh
+        let _ = std::process::Command::new("sh")
+            .arg(format!("./autogen.sh"))
+            .arg("--enable-static")
+            .arg(format!("--prefix={}", canonicalize(build_path.clone()).unwrap().display()))
+            .arg(format!("--build={}", host_triple))
+            .arg(format!("--host={}", target_triple))
+            .status();
+        // Run make
+        let _ = std::process::Command::new("make").status();
+        let _ = std::process::Command::new("make").arg("install").status();
+        // Add the include path to $CLFLAGS
+        add_cflag(&format!("-I{}", canonicalize(build_path.join("include")).unwrap().display()));
+        // Add lib to ldlibs
+        add_ldlib(&format!("-L{}", canonicalize(build_path.join("lib")).unwrap().display()));
+        // Change directory back to project root
+        let _ = env::set_current_dir("../../");
+
+        ////////////////////////////
+        // LIBIMOBILED-GLUE BUILD //
+        ////////////////////////////
+
+        // Change directory to libimobiledevice
+        let _ = env::set_current_dir("./submodules/libimobiledevice-glue");
+        // Create build folder
+        let _ = create_dir("./build");
+        let build_path = PathBuf::from("./build");
+        // Run ./autogen.sh
+        let _ = std::process::Command::new("sh")
+            .arg(format!("./autogen.sh"))
+            .arg("--enable-static")
+            .arg(format!("--prefix={}", canonicalize(build_path.clone()).unwrap().display()))
+            .arg(format!("--build={}", host_triple))
+            .arg(format!("--host={}", target_triple))
+            .status();
+        // Run make
+        let _ = std::process::Command::new("make").status();
+        let _ = std::process::Command::new("make").arg("install").status();
+        // Add the include path to $CLFLAGS
+        add_cflag(&format!("-I{}", canonicalize(build_path.join("include")).unwrap().display()));
+        // Add lib to ldlibs
+        add_ldlib(&format!("-L{}", canonicalize(build_path.join("lib")).unwrap().display()));
+        // Change directory back to project root
+        let _ = env::set_current_dir("../../");
+
+        ////////////////////////////
+        // LIBIMOBILEDEVICE BUILD //
+        ////////////////////////////
+
+        // Change directory to libimobiledevice
+        let _ = env::set_current_dir("./submodules/libimobiledevice");
+        // Create build folder
+        let _ = create_dir("./build");
+        let build_path = PathBuf::from("./build");
+        // Run ./autogen.sh
+        let _ = std::process::Command::new("sh")
+            .arg(format!("./autogen.sh"))
+            .arg("--enable-static")
+            .arg(format!("--prefix={}", canonicalize(build_path.clone()).unwrap().display()))
+            .arg(format!("--build={}", host_triple))
+            .arg(format!("--host={}", target_triple))
+            .status();
+        // Run make
+        let _ = std::process::Command::new("make").arg(format!("LDLIBS={}", ldlibs)).status();
+        let _ = std::process::Command::new("make").arg("install").status();
+        // Add the include path to $CLFLAGS
+        add_cflag(&format!("-I{}", canonicalize(build_path.join("include")).unwrap().display()));
+        // Change directory back to project root
+        let _ = env::set_current_dir("../../");
+
+        ////////////////////////////
+        //      GNUTLS BUILD      //
+        ////////////////////////////
+
+        // Change directory to libimobiledevice
+        let _ = env::set_current_dir("./submodules/gnutls");
+        // Create build folder
+        let _ = create_dir("./build");
+        let build_path = PathBuf::from("./build");
+        // Run boostrap
+        let _ = std::process::Command::new("sh")
+            .arg(format!("./bootstrap.sh")).status();
+        // Run ./configure.sh
+        let _ = std::process::Command::new("sh")
+            .arg(format!("./configure.sh"))
+            .arg("--enable-static")
+            .arg(format!("--prefix={}", canonicalize(build_path.clone()).unwrap().display()))
+            .arg(format!("--build={}", host_triple))
+            .arg(format!("--host={}", target_triple))
+            .status();
+        // Run make
+        let _ = std::process::Command::new("make").status();
+        let _ = std::process::Command::new("make").arg("install").status();
+        // Add the include path to $CLFLAGS
+        add_cflag(&format!("-I{}", canonicalize(build_path.join("include")).unwrap().display()));
+        // Change directory back to project root
+        let _ = env::set_current_dir("../../");
+
+        panic!();
     }
 
     // Link libi* deps
@@ -103,4 +216,18 @@ fn main() {
         "macos" => println!("cargo:rustc-link-search=/opt/homebrew/opt/openssl@3/lib"),
         _ => panic!("Unsupported OS"),
     };
+}
+
+fn add_cflag(flag: &str) {
+    let mut flags = env::var("CFLAGS").unwrap_or_default();
+    flags.push_str(" ");
+    flags.push_str(flag);
+    env::set_var("CFLAGS", flags);
+}
+
+fn add_ldlib(flag: &str) {
+    let mut flags = env::var("LDFLAGS").unwrap_or_default();
+    flags.push_str(" ");
+    flags.push_str(flag);
+    env::set_var("LDFLAGS", flags);
 }
