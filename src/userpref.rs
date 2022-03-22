a736cfd30155cceaefbc841e6bad9289e50738c5
jkcoxson <jkcoxson@gmail.com>
2022-03-22 14:44:58 -0600

    Remove debugging prints for userpref

--- src/userpref.rs
+++ src/userpref.rs
@@ -9,10 +9,8 @@ use crate::plist::Plist;
 pub fn read_pair_record(udid: String) -> Result<Plist, UserPrefError> {
     let udid = CString::new(udid).unwrap();
     let mut to_fill = unsafe { std::mem::zeroed() };
-    println!("segfault?");
     let results =
         unsafe { unsafe_bindings::userpref_read_pair_record(udid.as_ptr(), &mut to_fill) }.into();
-    println!("nope");
     if results != UserPrefError::Success {
         return Err(results);
     }
