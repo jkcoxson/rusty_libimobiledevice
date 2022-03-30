5ddbd5f85d101c7101526d6bc032c8c95b93c1f4
Jackson Coxson <admin@jkcoxson.com>
2022-03-30 14:09:01 -0600

    Finish debug server

--- src/debug_server.rs
+++ src/debug_server.rs
@@ -2,6 +2,8 @@
 
 use std::convert::TryInto;
 
+use libc::c_int;
+
 use crate::{
     bindings as unsafe_bindings, debug, error::DebugServerError, libimobiledevice::Device,
 };
@@ -41,6 +43,118 @@ impl DebugServer<'_> {
             pointer: unsafe { *client_ptr },
             phantom: std::marker::PhantomData,
         })
+    }
+
+    pub fn send(&self, data: String) -> Result<(), DebugServerError> {
+        let data_c_str = std::ffi::CString::new(data).unwrap();
+        let mut sent = 0;
+        let result = unsafe {
+            unsafe_bindings::debugserver_client_send(
+                self.pointer,
+                data_c_str.as_ptr(),
+                data_c_str.as_bytes().len().try_into().unwrap(),
+                &mut sent
+            )
+        }
+        .into();
+        if result != DebugServerError::Success {
+            return Err(result);
+        }
+
+        Ok(())
+    }
+
+    pub fn recieve_with_timeout(&self, size: u32, timeout: u32) -> Result<String, DebugServerError> {
+        let mut data = vec![0u8; size as usize];
+        let mut received = 0;
+        let result = unsafe {
+            unsafe_bindings::debugserver_client_receive_with_timeout(
+                self.pointer,
+                data.as_mut_ptr() as *mut i8,
+                size,
+                &mut received,
+                timeout
+            )
+        }
+        .into();
+        if result != DebugServerError::Success {
+            return Err(result);
+        }
+
+        Ok(String::from_utf8(data).unwrap())
+    }
+
+    pub fn recieve(&self, size: u32) -> Result<String, DebugServerError> {
+        let mut data = vec![0u8; size as usize];
+        let mut received = 0;
+        let result = unsafe {
+            unsafe_bindings::debugserver_client_receive(
+                self.pointer,
+                data.as_mut_ptr() as *mut i8,
+                size,
+                &mut received
+            )
+        }
+        .into();
+        if result != DebugServerError::Success {
+            return Err(result);
+        }
+
+        Ok(String::from_utf8(data).unwrap())
+    }
+
+    pub fn recieve_response(&self) -> Result<String, DebugServerError> {
+        let mut data = unsafe {
+            std::mem::zeroed()
+        };
+        let mut size = 0;
+        let result = unsafe {
+            unsafe_bindings::debugserver_client_receive_response(
+                self.pointer,
+                &mut data,
+                &mut size,
+            )
+        }
+        .into();
+        if result != DebugServerError::Success {
+            return Err(result);
+        }
+        let data = data as *mut u8;
+
+        Ok(String::from_utf8(unsafe { std::slice::from_raw_parts(data, size as usize).to_vec() }).unwrap())
+    }
+
+    pub fn set_ack_mode(&self, enabled: bool) -> Result<(), DebugServerError> {
+        let result = unsafe {
+            unsafe_bindings::debugserver_client_set_ack_mode(
+                self.pointer,
+                enabled as c_int
+            )
+        }
+        .into();
+        if result != DebugServerError::Success {
+            return Err(result);
+        }
+
+        Ok(())
+    }
+
+    pub fn set_environment_hex_encoded(&self, env: String) -> Result<String, DebugServerError> {
+        let env_c_str = std::ffi::CString::new(env).unwrap();
+        let mut response = unsafe { std::mem::zeroed() };
+        let result = unsafe {
+            unsafe_bindings::debugserver_client_set_environment_hex_encoded(
+                self.pointer,
+                env_c_str.as_ptr(),
+                &mut response
+            )
+        }
+        .into();
+        if result != DebugServerError::Success {
+            return Err(result);
+        }
+
+        Ok(unsafe { std::ffi::CStr::from_ptr(response).to_string_lossy().into_owned() })
     }
 
     pub fn send_command(&self, command: DebugServerCommand) -> Result<String, DebugServerError> {
@@ -112,6 +226,46 @@ impl DebugServer<'_> {
         };
 
         Ok(response_str)
+    }
+
+    pub fn encode_string(buffer: String) -> (String, u32) {
+        let mut encoded_buffer = unsafe { std::mem::zeroed() };
+        let mut encoded_buffer_size = 0;
+        let buffer_c_str = std::ffi::CString::new(buffer).unwrap();
+        let buffer_ptr = buffer_c_str.as_ptr() as *mut i8;
+        unsafe {
+            unsafe_bindings::debugserver_encode_string(
+                buffer_ptr,
+                &mut encoded_buffer,
+                &mut encoded_buffer_size,
+            );
+        }
+        let encoded_buffer_str = unsafe {
+            std::ffi::CStr::from_ptr(encoded_buffer)
+                .to_string_lossy()
+                .to_string()
+        };
+        (encoded_buffer_str, encoded_buffer_size as u32)
+    }
+
+    pub fn decode_string(buffer: String) -> String {
+        let mut decoded_buffer = unsafe { std::mem::zeroed() };
+        let buffer_len = buffer.len() as u64;
+        let buffer_c_str = std::ffi::CString::new(buffer).unwrap();
+        let buffer_ptr = buffer_c_str.as_ptr() as *mut i8;
+        unsafe {
+            unsafe_bindings::debugserver_decode_string(
+                buffer_ptr,
+                buffer_len,
+                &mut decoded_buffer,
+            );
+        }
+        let decoded_buffer_str = unsafe {
+            std::ffi::CStr::from_ptr(decoded_buffer)
+                .to_string_lossy()
+                .to_string()
+        };
+        decoded_buffer_str
     }
 }
 
