c77321d7ea422c517e4db72781266a909c6baf13
Jackson Coxson <admin@jkcoxson.com>
2022-04-08 06:52:21 -0600

    Add basic documentation of modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 #[doc = include_str!("../README.md")]
 
 mod bindings;
+/// TODO
 pub mod callback;
+/// A debug macro used throughout the crate
+pub mod debug;
+/// A module containing all possible errors produced by the library
 pub mod error;
+/// Creates connections and manages high level interfaces for iOS devices
 pub mod idevice;
+/// Plist is Apple's human readable format for storing data. Will be moved soon tm
 pub mod plist;
-pub mod preference_plist;
+/// A bare bones representation of a service running on a device. 
+/// Useful for services that don't have modules or for running raw commands
 pub mod service;
+/// A module that contains all abstractions for built-in services
 pub mod services;
--- src/services.rs
+++ src/services.rs
@@ -1,25 +1,49 @@
 // jkcoxson
 
+/// Transfers files between host and the iDevice
 pub mod afc;
+/// Usage unknown
 pub mod companion_proxy;
+/// Usage unknown
 pub mod connection;
-pub mod debug;
+/// Used for debugging applications on the device
 pub mod debug_server;
+/// Relays diagnostic logs from the iOS device to the host
 pub mod diagnostics_relay;
+/// Relays files from the iOS device to the host
 pub mod file_relay;
+/// A required service for most other services. 
+/// iOS will close other connections if there is no active heartbeat client
 pub mod heartbeat;
+/// iTunes file transfer service
 pub mod house_arrest;
+/// Manages installing, removing and modifying applications on the device
 pub mod instproxy;
+/// A jumping point for other services
 pub mod lockdownd;
+/// Manges and checks provisioning profiles
 pub mod misagent;
+/// Activates the device for iCloud Activation
 pub mod mobile_activation;
+/// Manages backups of the iOS device's data
+/// Contains mobilebackup for < iOS 4 and mobilebackup2 for >= iOS 4
 pub mod mobile_backup;
+/// iTunes mobile sync service
 pub mod mobile_sync;
+/// Sends and watches for notifications on the iDevice
 pub mod notification_proxy;
+/// A service that manages data at the first unlock screen after boot.
+/// Prepare to be boarded!
 pub mod preboard;
+/// Usage unknown
 pub mod property_list_service;
+/// Restores an iDevice to a specific backup or iOS version
 pub mod restored;
+/// Takes a screenshot and returns it to the host
 pub mod screenshotr;
+/// Manages the device's OS base and homescreen.
 pub mod springboard_services;
+/// The iOS device's settings. Very fun to mess with.
 pub mod userpref;
+/// First used on MacOS, this service is used to inspect the JavaScript and HTML of a site running on the device
 pub mod web_inspector;
